9f59be1b4d19fbee533f2abecec3f46d4f7b3870
Max <maksim@tsvetovat.org>
2011-09-01 18:38:57 -0400

    First commit of Hello World
    Signed-off-by: Max <maksim@tsvetovat.org>

---
+++ Hello/src/hello/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author IN330FAC
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
